038e287314b0e13672212c57cb35017a08321e59
Eby <ethomasa@gmail.com>
2021-11-29 13:47:30 -0600

    components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen'
 
 const App= ()=> {
   return (
     <>
- <h1> Welocme To ProShop </h1>
+    <Header/>
+    <main className='py-3'>
+      <Container>
+  
+      <HomeScreen/>
+        </Container>
+
+ </main>
+ <Footer/>
     </>
   );
 }
