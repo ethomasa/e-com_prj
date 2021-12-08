49e1e38baa72f8747bbd39dfc8b0ecc5ad8347b6
Eby <ethomasa@gmail.com>
2021-12-08 16:55:38 -0600

    fixing deploy issues

--- server/server.js
+++ server/server.js
@@ -36,6 +36,19 @@ app.get('/api/config/paypal', (req, res) =>
 
 
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+
 
   db.once('open', () => {
  app.listen(PORT,  () => {
